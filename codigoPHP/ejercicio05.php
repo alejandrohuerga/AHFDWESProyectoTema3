c2793a110f428c52927456adc42103b4873ab7af
alejandro.dev <alejandro.dev@gmail.com>
2025-11-04 13:41:04 +0100

    MEJORA EN EJERCICIOS TEMA 3 - Se han mejorado los ejercicios 5 Y 6.

--- codigoPHP/ejercicio05.php
+++ codigoPHP/ejercicio05.php
@@ -6,21 +6,31 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Ejercicio 05 - Alejandro De la Huerga</title>
     </head>
     <body>
+        <h1>Alejandro De la Huerga</h1>
+        <h2>Ejercicio 5</h2>
         <?php
         /**
-         * @author: Alejandro De la Huerga
-         * @since: 13/10/2025
-         * 5. Inicializar y mostrar una variable que tiene una marca de tiempo (timestamp).
+         * @author:  Alejandro De la Huerga
+         * @since  04/11/2025
+         * 
          */
-        
-        $oFecha=new DateTime("now");
-        
-        // Convertimos la fecha actual a timestamp.
-        print date_timestamp_get($oFecha);
-        
+        // Establecer la zona horaria 
+            date_default_timezone_set('Europe/Madrid');
+        //Para obtener el timestamp actual
+        //  $timeStamp = time();
+        //Mostrar el timeStamp
+        // echo "El Timestamp actual es: ".date("d-m-Y H:i:s ",$timeStamp);
+
+
+            $ofecha = new DateTime();
+        //Para obtener el timestamp actual
+            $timestamp = $ofecha->getTimestamp();
+        //Mostrar el timeStamp
+            echo"<p>Codigo TimeStamp : " . $timestamp . "</p>";
+            echo '<p class="parafo">El formato legible del Timestamp actual es: ' . date("d-m-Y H:i:s", $timestamp) . "</p>";
         ?>
     </body>
 </html>
--- codigoPHP/ejercicio06.php
+++ codigoPHP/ejercicio06.php
@@ -9,26 +9,49 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <title></title>
     </head>
     <body>
-        <?php
-        /**
-         * @author: Alejandro De la Huerga
-         * @since: 13/10/2025
-         * 6. Inicializar y mostrar una variable que tiene una marca de tiempo (timestamp).
-        */
-        echo("<h1>Alejandro De la Huerga</h1>");
-        
-        $oFecha= new DateTime();
-        
-        //Mostramos la fecha actual.
-        echo("<h2>Fecha actual</h2>");
-        echo($oFecha->format("d/m/Y"));
-        
-        // Le agregamos 60 dias con el meto add.
-        $oFecha->add(new DateInterval('P60D'));
-        
-        //Mostramos la fecha dentro de 60 dias.
-        echo("<h2>Fecha dentro de 60 dias</h2>");
-        echo($oFecha->format("d/m/Y"));
-        ?>
+        <header class="header">
+            <h1>Ejercicio 06</h1>
+        </header>
+        <main>
+            <section>
+                <?php
+                /**
+                 * @author: Alejandro De la Huerga
+                 * @since 04/11/2025
+                 * 
+                 */
+                //Operar con fechas: calcular la fecha y el día de la semana de dentro de 60 días.
+                // Establecer la zona horaria 
+                date_default_timezone_set('Europe/Madrid');
+                $ofecha = new DateTime();
+                // Establecer el locale (idioma) en español
+                setlocale(LC_TIME, 'es_ES.UTF-8', 'es_ES', 'spanish');
+                https://www.php.net/manual/es/class.dateinterval.php
+                //Intervalo de días
+
+                $intervalo = new DateInterval('P60D');
+
+                $ofecha->add($intervalo);
+                echo('<br><br><h3>Fecha dentro de 60 días</h3> ');
+                echo( "<p>La fecha de hoy es :<span> " . $ofecha->format("l") . " " . $ofecha->format("d") . " de " . $ofecha->format("F") . " de " . $ofecha->format("o") . " y la hora es: " . $ofecha->format("H:i:s") . '</span></p>');
+
+                echo('<br><br><h3>Usando el timestamp de DateTime y strftime, los dias y los meses están en español</h3> ');
+                echo "<p>La fecha dentro de 60 días será : <span>" . strftime("%A %d de %B de %Y", $ofecha->getTimestamp()) .
+                " y la hora es: " . $ofecha->format("H:i:s") . '</span></p>';
+                ?>
+            </section>
+
+
+        </main>
+        <footer class="footer">
+            <div class="footerContent">
+                <div><p class="copyright">
+                        2025-26 IES LOS SAUCES. &#169;Todos los derechos reservados.</p> <address><a href="../indexProyectoTema3.php">Alejandro De la Huerga.</a> Fecha de Actualización :
+                        <time datetime="2025-11-04"></time> 04-11-2025 </address>
+                </div>
+
+            </div>
+
+        </footer>
     </body>
 </html>
